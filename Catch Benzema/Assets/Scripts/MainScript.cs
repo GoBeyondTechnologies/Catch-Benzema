98d8566adacd9f8b76b34b44e9364a7c1b75e55d
AvivS9 <avivshlomi@gmail.com>
2020-10-23 22:41:56 +0300

    Players running on field

---
+++ Catch Benzema/Assets/Scripts/MainScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class MainScript : MonoBehaviour
+{
+
+    public GameObject benzema;
+    public GameObject player;
+    public Text speedtext;
+
+   
+
+    public float benzemaspeed;
+    public float playerspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        benzemaspeed = benzema.GetComponent<ThirdPersonUserControl>().speed;
+        playerspeed = player.GetComponent<ThirdPersonUserControl>().speed;
+
+        speedtext.text = benzemaspeed.ToString() + "\n" + playerspeed.ToString();
+    }
+
+    public void updatespeed()
+    {
+        player.GetComponent<ThirdPersonUserControl>().changespeed(0.01f);
+    }
+}
